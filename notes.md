a7bb2cadb0c9cfa5527185c5a2b5020e8fd5b51a
Abel Woldemichael <abelllselam@gmail.com>
2025-04-26 18:06:21 -0400

    just finished the update in the play.js file

--- notes.md
+++ notes.md
@@ -95,7 +95,7 @@ const bookSchema = new mongoose.Schema({ title: String, price: Number });
 const Book = mongoose.model("Book", bookSchema); // ← model
 ```
 
-- A model in simple terms is a JS class that you use to talk to one MongoDB collection. It is build from a schema and it gives you helper methods like create, find, update and delete. By default the model is simgular name like: Item and maps to a plural collection items in MOngoDB.
+- A model in simple terms is a JS class that you use to talk to one MongoDB collection. It is build from a schema and it gives you helper methods like create, find, update and delete. By default the model is singular name like: Item and maps to a plural collection items in MOngoDB.
   - Please check the Item.js inside models folder for further information.
 - Think 3 layers in a typical Node backend:
   Layer Main role Example folder
--- tests/play.js
+++ tests/play.js
@@ -7,12 +7,22 @@ dotenv.config();
 (async () => {
   // 1️⃣ connect (give Atlas up to 30 s to wake up)
   await mongoose.connect(process.env.MONGODB_URI, {
-    serverSelectionTimeoutMS: 30000,
+    serverSelectionTimeoutMS: 30000, //sets how long it will wait for that handshake to succeed before throwing MongooseServerSelectionError.Free or sleepy Atlas clusters sometimes take longer than the default (10 s), so bumping it to 30 s prevents premature timeouts.
   });
 
   // 2️⃣ CREATE one document
   const saved = await Item.create({ name: "AW coffee", price: 10 });
   console.log("Saved:", saved);
+
+  const updatedItem = await Item.updateOne(
+    { _id: "680d4861c321dc0121dd1c65" },
+    {
+      name: "Changed from AW coffee",
+      price: 20,
+    }
+  );
+
+  console.log("This is update: ", updatedItem);
 
   // 3️⃣ READ them all back
   const all = await Item.find();
@@ -20,4 +30,6 @@ dotenv.config();
 
   // 4️⃣ disconnect so Node exits
   await mongoose.disconnect();
-})().catch(console.error);
+})().catch(console.error); //(async () => { /* ... */ })
+//Creates an anonymous async function but doesn’t run it yet.The trailing () immediately afterward
+//... })()  → invokes that function right away and returns its Promise.
